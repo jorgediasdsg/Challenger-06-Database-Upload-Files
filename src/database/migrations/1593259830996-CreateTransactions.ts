5c5139a0028676e76cb02ae07a83a3e61920ec78
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-27 09:11:23 -0300

    [ ] Migrations

---
+++ src/database/migrations/1593259830996-CreateTransactions.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTransactions1593259830996 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
