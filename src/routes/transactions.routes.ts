48f88e47a3f01289a9774ebe3daf6f658d94ae76
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-27 10:30:38 -0300

    [ ] Services/CreateTransactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -12,7 +12,17 @@ transactionsRouter.get('/', async (request, response) => {
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction({
+    title,
+    value,
+    type,
+    category,
+  });
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
