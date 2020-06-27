30be48d9aa971f99b68f3a0671d0b85b31530883
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-27 09:50:38 -0300

    [ ] Migrations/categories

---
+++ src/database/migrations/1593262221700-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategories1593262221700 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
