b48d00b62fa6142305b1c95fb94b5df2d62ddc8f
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-27 10:33:18 -0300

    [ ] Services/CreateTransactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -2,8 +2,20 @@
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
     // TODOoo
   }
 }
